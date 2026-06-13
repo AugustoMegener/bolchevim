f7a1ad987d8f3537f1e9b83c910b548736b54a81
AugustoMegener <76854495+AugustoMegener@users.noreply.github.com>
2026-06-13 09:59:41 -0300

    Feat(conceals): conceals added

--- config/core/options.nix
+++ config/core/options.nix
@@ -118,7 +118,7 @@ local function apply(bufnr, top, bot)
           vim.api.nvim_buf_set_extmark(bufnr, ns, lnum, start - 1, {
             end_col = finish,
             virt_text = {{ entry.icon, "" }},
-            virt_text_pos = "inline",
+            virt_text_pos = "overlay",
             conceal = "",
             ephemeral = true,
           })
